3d878d70a9aa5f4727140b93d0ebba722ebdfaaf
Kailin <67965369+kailinxGitHub@users.noreply.github.com>
2024-03-17 04:50:27 -0700

    update urls for chat

--- Backend/views/chat/urls.py
+++ Backend/views/chat/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from . import index, list
+from . import index, comment, list
 
 urlpatterns = [
     path('', index.Handler.as_view()),
+    path('comment/', comment.Handler.as_view()),
     path('list/', list.Handler.as_view()),
 ]

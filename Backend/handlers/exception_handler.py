32dd0d24f9578273c8941a26ede7487422f6ebfa
tanyuzhou <tyz@gztyz.com>
2024-03-17 00:52:01 -0700

    update: res param key

--- Backend/handlers/exception_handler.py
+++ Backend/handlers/exception_handler.py
@@ -7,8 +7,8 @@ def custom_exception_handler(exc, context):
     if response is not None:
         # Customize the error message format
         customized_data = {
-            'status': 1, 
-            'msg': response.data.get('detail', 'Internal Server Error'), 
+            'code': 1, 
+            'message': response.data.get('detail', 'Internal Server Error'), 
         }
         response.data = customized_data
 
